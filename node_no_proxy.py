9954adf82a9792c4b32430aaa7c51602edce4319
isnullibi <isnullibi@gmail.com>
2024-07-23 19:00:36 +0300

    Version upgrade

--- node_no_proxy.py
+++ node_no_proxy.py
@@ -74,7 +74,7 @@ async def connect_to_wss(user_id):
                                 "user_agent": custom_headers['User-Agent'],
                                 "timestamp": int(time.time()),
                                 "device_type": "extension",
-                                "version": "4.20.2",
+                                "version": "4.26.2",
                                 "extension_id": "lkbnfiajjmbhnfledhphioinpickokdi"
                             }
                         }
